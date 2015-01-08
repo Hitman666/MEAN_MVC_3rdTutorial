77e28be38b9330c9a5b71af282bfc70ef24b0fb3
Hitman666 <nikola.breznjak@gmail.com>
2015-01-08 23:14:03 +0100

    added and configured passport

--- config/express.js
+++ config/express.js
@@ -1,36 +1,15 @@
 var config = require('./config'),
 	express = require('express'),
-	//morgan = require('morgan'),
-	//compress = require('compression'),
 	bodyParser = require('body-parser');
-	//methodOverride = require('method-override'),
-	//session = require('express-session')
 
 module.exports = function() {
 	var app = express();
-	
-	// if (process.env.NODE_ENV === 'development') {
-	// 	app.use(morgan('dev'));
-	// }
-	// else if (process.env.NODE_ENV === 'production') {
-	// 	app.use(compress());
-	// }
 
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
 
-
 	app.use(bodyParser.json());
-
-	//app.use(methodOverride());
-
-	// app.use(session({
-	// 	saveUninitialized: true,
-	// 	resave: true,
-	// 	secret: config.sessionSecret
-	// }));
-
 
 	app.set('views', './app/views');
 	app.set('view engine', 'ejs');
